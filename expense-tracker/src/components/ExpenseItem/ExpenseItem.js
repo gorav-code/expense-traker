9d34a25c8daa4ae9773fb9f52f4321967f6073d8
gorav-code <30561844+gorav-code@users.noreply.github.com>
2023-10-05 19:55:21 +0530

    all completed

--- expense-tracker/src/components/ExpenseItem/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem/ExpenseItem.js
@@ -16,5 +16,19 @@ function ExpenseItem(props) {
     );
 }
 
+const ExpenseItem = (props)  => {
+    return (
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}></ExpenseDate>
+                <div className='expense-item__description'>
+                    <h2 >{props.title}</h2>
+                    <div className='expense-item__price'>${props.amount}</div>
+                </div>
+            </Card>
+        </li>
+    );
+}
+
 export default ExpenseItem;
 
