75a6b8f4289e21346529cf62d0e92337e30964b8
gorav-code <30561844+gorav-code@users.noreply.github.com>
2023-10-06 16:31:14 +0530

    New expense item form added

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import Card from './components/Card/Card';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   return (
     <div>
       <h1>Lets gets started!</h1>
       <Card className="expenses">
-        {
-          <Expenses />
-        }
+        <NewExpense />
+        <Expenses />
       </Card>
     </div>
   );
