4292aa6800467fac9bab488a0dd43c26af437a2d
gorav-code <30561844+gorav-code@users.noreply.github.com>
2023-10-07 16:45:35 +0530

    Maintaining global expenses source list.

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -6,31 +6,33 @@ import NewExpense from './components/NewExpense/NewExpense';
 import ExpenseFilter from './components/Expenses/ExpenseFilter/ExpenseFilter';
 import React, { useState } from 'react';
 
+const expensesOriginal = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+]
+
 function App() {
   const [filterYear, setFilterYear] = useState('2020');
   const [filterInfoText, setFilterInfoText] = useState('2019, 2021 & 2022');
 
-  const [expenses, setExpenses] = useState([
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ]);
+  const [expenses, setExpenses] = useState(expensesOriginal);
 
 
   const addExpenseHandler = (expense) => {
@@ -38,9 +40,13 @@ function App() {
     console.log(expense);
 
     //add to expenses list
-    //expenses.push(expense);
-
-    setExpenses(expense);
+    //expensesOriginal.push(expense);
+    
+    //Note: since we are updating our state based on previous state then we will use function form 
+    setExpenses(prevExpenses => {
+      //this will return a new array by merging new item from 'setExpenses' and existing items from '...expenses'.
+      return [expense, ...prevExpenses]
+    });
   }
 
 
